217dc2932cca3c868b8c11e8b6cef9904be05554
ZacariTest <zacaritest@gmail.com>
2024-05-15 20:50:50 -0300

    Añadir parte del diagrama de cuenta

--- cajero/src/cajero/Cuenta.java
+++ cajero/src/cajero/Cuenta.java
@@ -2,6 +2,31 @@ package cajero;
 
 public class Cuenta {
 
+    private String ubicacion;
+    private Double saldo;
+
+    // Constructor
+    public Cuenta(String ubicacion, Double saldo) {
+        this.ubicacion = ubicacion;
+        this.saldo = saldo;
+    }
 
 
+    // Métodos getter y setter para ubicación
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    // Métodos getter y setter para saldo
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
 }
